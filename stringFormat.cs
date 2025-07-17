a637050061a36e9de6c2947f83a302a1a6083db7
Sidhant Malik <96983080+sidhant2094@users.noreply.github.com>
2025-07-17 12:19:47 +0530

    Create stringFormat.cs

---
+++ stringFormat.cs
@@ -0,0 +1,7 @@
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
